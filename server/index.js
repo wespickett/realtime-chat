e9a128889093e2a6b04fe7127249372963e79872
Wes Pickett <wes@krossover.com>
2015-08-02 12:36:06 -0400

    serve app from node.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var socketIO = require('socket.io')(http);
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
+app.use(express.static('../client'));
+
+ app.get('/', function(req,res) {
+    res.sendFile(__dirname + '/index.html');
+ });
+
 socketIO.on('connection', function(socket){
+  console.log('a user connected');
 
   socket.on('newChatMessage', function(msg){
+    console.log(msg);
     socketIO.emit('chatMessage', msg);
   });
 });
